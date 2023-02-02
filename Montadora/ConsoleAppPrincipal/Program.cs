ad6cc8d9fd8da6ce522b60c967bd78b4cb2c8718
A.Barbosa <18252705+arbarbosa1979@users.noreply.github.com>
2023-02-01 21:32:40 -0300

    Update Program.cs

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -7,26 +7,19 @@ namespace ConsoleAppPrincipal
         static void Main(string[] args)
         {
             Pneus Pneu1 = new Pneus(16, 150, "Carro de Passeio", false);
-            Pneu1.Aro = 16;
-            Pneu1.VelocidadeMaxima = 100;
-            Pneu1.Estepe = false;
-            Pneu1.Tipo = "Carro de passeio";
 
-
-            Pneus Pneu2 = new Pneus();
-            Pneu2.Aro = 16;
-            Pneu1.VelocidadeMaxima = 70;
-            Pneu1.Estepe = true;
-            Pneu1.Tipo = "Pneu de Estepe";
+            Pneus Pneu2 = new Pneus(16, 70, "Pneu de Estepe", true);
 
             Pneu1.Girar(6);
             Pneu2.Girar(6);
-            Pneu2.Girar(6);
-            Pneu2.Girar(6);
+            Pneu2.Girar(15);
+            Pneu2.Girar(10);
             Pneu2.Frear(5);
-
+            Pneu2.Girar(20);
+            Pneu2.Girar(20);
+            Pneu2.Girar(5);
             Pneu1.Exibir();
-
+            Console.WriteLine();
             Pneu2.Exibir();
 
         }
